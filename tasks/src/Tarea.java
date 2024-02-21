bcbe30960e8a94efc8d5080716ef19c5cbfb7e36
DAGod <90727076+inaDAGod@users.noreply.github.com>
2024-02-21 20:43:40 +0000

    base clase Tarea

---
+++ tasks/src/Tarea.java
@@ -0,0 +1,25 @@
+public class Tarea{
+    private String nombre;
+    private String detalles;
+    private String estado;
+
+    public Tarea(String nombre, String detalles, String estado){
+        this.nombre = nombre;
+        this.detalles = detalles;
+        this.estado =estado;
+    }
+
+    public String getNombre() {
+		return nombre;
+	}
+    public String getDetalles() {
+		return detalles;
+	}
+    public String getEstado() {
+		return estado;
+	}
+
+
+    
+
+}
