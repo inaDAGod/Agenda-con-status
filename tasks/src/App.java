617e58320dc304f95d14ad49e74048ef3ad9edec
inaDAGod <danialee14@gmail.com>
2024-02-22 00:10:30 -0400

    correciones clases

--- tasks/src/App.java
+++ tasks/src/App.java
@@ -1,5 +1,12 @@
+import java.time.LocalDate;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Tarea t= new Tarea("nombre", "detalle", LocalDate.now());
+        System.out.println(t);
+        Registro r = new Registro();
+        r.addTarea(t);
+        r.addTarea(t);
+        System.out.println(r);
     }
 }
--- tasks/src/Registro.java
+++ tasks/src/Registro.java
@@ -1,25 +1,33 @@
 import java.util.ArrayList;
 
 public class Registro{
-    private ArrayList<Fecha> fechas;
+    private ArrayList<Tarea> tareas;
 
-    public Registro(){
-        this.fechas =new ArrayList<Fecha>();
-    }
+	public Registro() {
+		this.tareas = new ArrayList<>();
+	}
 
-    public ArrayList getFechas(){
-        return fechas;
-    }
+	public ArrayList<Tarea> getTareas() {
+		return tareas;
+	}
 
-    public void addFecha(Fecha fecha){
-        fechas.add(fecha);
-    }
+	public void setTareas(ArrayList<Tarea> tareas) {
+		this.tareas = tareas;
+	}
+	
+	public void addTarea(Tarea tarea) {
+		tareas.add(tarea);
+	}
 
-    @Override
-    public String toString() {
-        // TODO Auto-generated method stub
-        return super.toString();
-    }
-
-
+	@Override
+	public String toString() {
+		String s = "";
+		for(Tarea t: tareas) {
+			s += t.toString();
+		}
+		return s;
+	}
+	
+	
+    
 }
